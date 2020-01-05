b76428daa4dbc2bc97971fddf629542ce23e3167
Gerardo Enrique Arriaga Rendon <54829718+GerardoArriaga@users.noreply.github.com>
2020-01-04 21:51:05 -0500

    Add files via upload

---
+++ P#2/p2.c
@@ -0,0 +1,30 @@
+/*
+	Project Euler P#2
+	
+	Written by:
+	Gerardo Enrique Arriaga Rendon
+*/
+#include <stdio.h>
+#define LMT 4000000
+
+int main(void)
+{
+	int f_prev = 0, f_crnt = 1, sum = 0, f_next;
+
+	do
+	{
+		if (f_crnt % 2 == 0)
+		{
+			sum += f_crnt;
+		}
+		
+		f_next = f_crnt + f_prev;
+		f_prev = f_crnt;
+		f_crnt = f_next;
+
+	} while (f_next <= LMT);
+
+	printf("The sum of all even Fibonacci numbers less than %d is %d.\n", LMT, sum);
+	
+	return 0;
+}
